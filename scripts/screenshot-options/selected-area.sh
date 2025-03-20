47f3a06011943bc42fb99f046725f2816dfea843
alejo-c <4lejo.castrillon@gmail.com>
2025-03-19 20:44:56 -0500

    Create screenshot menu

--- scripts/screenshot-options/selected-area.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$HOME/pictures/screenshots/"
-NAME="screenshot_$(date +%d%m%Y_%H%M%S).png"
-
-grim -g "$(slurp)" "$DIR$NAME"
-notify-send "Screenshot created and copied to clipboard" "Mode: Selected area"
-swappy -f "$DIR$NAME"
