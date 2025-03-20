20c448bb0b9d573ebd5dddcbe7712822c1b4ec38
alejo-c <4lejo.castrillon@gmail.com>
2025-03-19 20:49:19 -0500

    Update rofi menus and scripts

--- .config/rofi/drun.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rofi -show drun -replace -i
-# & sleep 0.2; hyprctl dispatch focuswindow "^(Rofi)"
--- .config/rofi/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
---no-startup-id rofi -show drun -run-shell-command 'st -ic "{cmd} && read"'
