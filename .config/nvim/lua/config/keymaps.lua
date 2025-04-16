89d47eee569791d4002001240af9e70da47db22b
alejo-c <4lejo.castrillon@gmail.com>
2025-04-15 21:14:14 -0500

    Add new nvim keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,15 +1,16 @@
 local map = vim.keymap.set
 
 local keymaps = {
-  --{ { "n", "v" }, "n", "'Nn'[v:searchforward]" },
-  { { "n", "v" }, "<c-j>", "<c-w>j" },
-  { { "n", "v" }, "<c-k>", "<c-w>k" },
-  { { "n", "v" }, "<c-l>", "<c-w>l" },
-  { { "n", "v" }, "<c-h>", "<c-w>h" },
   { { "n", "v" }, "-", "^" },
   { { "n", "v" }, "_", "g_" },
+  { "n", "<leader>a", "gg<s-v>G" },
+  { "n", "<leader>w", ":w<cr>" },
+  { "n", "<leader>W", ":wa<cr>" },
+  { "n", "<leader>q", ":q<cr>" },
+  { "n", "<leader>qa", ":qa<cr>" },
+  { "n", "<leader>Q", ":q!<cr>" },
   -- { { "n", "v" }, "n", "'Nn'[v:searchforward] . 'zz'" },
-  { "n", "<c-a>", "gg<s-v>G" },
+  --{ { "n", "v" }, "n", "'Nn'[v:searchforward]" },
 }
 
 for _, v in pairs(keymaps) do
